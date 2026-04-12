91325ed94646602671946a02c4e9d3de441410ab
Vasil <vasil.domontovich@outlook.com>
2026-04-12 14:34:06 +0300

    Removed "zed xp multiplier" from /info command

--- discord-bot-script/index.js
+++ discord-bot-script/index.js
@@ -806,7 +806,6 @@ function formatInfoResponse(interaction, request, responsePayload) {
 
   const multipliers = [
     typeof info.zedsHpMultiplier === 'number' ? `Zed HP x${info.zedsHpMultiplier}` : null,
-    typeof info.zedsXpMultiplier === 'number' ? `Zed XP x${info.zedsXpMultiplier}` : null,
     typeof info.dmgMultiplier === 'number' ? `Damage x${info.dmgMultiplier}` : null,
   ].filter(Boolean);
   if (multipliers.length > 0) {
